c793d16ecabd29f01f885076cde914e6f8ac383c
Herman <herman.sollerman@gmail.com>
2025-07-04 10:49:50 +0200

    benchmark: added proble generation timerm

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,15 @@ use std::time;
 use stencil::{problems, typst_writer};
 fn main() {
     let now = time::SystemTime::now();
+    println!("Generating problems...");
     let problems = stencil::SetBuilder::new()
         .area(problems::SimpleLinearEquations)
-        .exclude(problems::SimpleLinearEquations::ONLY_MULTIPLICATION)
         .batch(stencil::Difficulty::Intro, 10)
         .build();
+    println!(
+        "Problems generated in {} s",
+        now.elapsed().unwrap_or_default().as_secs_f32()
+    );
 
     let message = problems.iter().next().unwrap().question();
     match typst_writer::write(message) {
