2fd827f3a284d8a249d3f61591e83f6449e8a07c
Herman <herman.sollerman@gmail.com>
2025-07-05 00:43:20 +0200

    refactor(Range): added support for custom exclusions for the range. Currently it can exclude 0, 1 and -1, for math formatting purposes. These are the numbers most likely to change a problem significantly

--- src/problems/range.rs
+++ src/problems/range.rs
@@ -3,39 +3,53 @@ use rand::seq::IndexedRandom;
 pub struct IntRange {
     min: i32,
     max: i32,
-    include_zero: bool,
+    exclude: Vec<i32>,
 }
 
 impl IntRange {
     pub fn with_zero(min: i32, max: i32) -> IntRange {
-        assert!(min <= max, "min must be smaller than max!");
-        IntRange {
-            min,
-            max,
-            include_zero: true,
-        }
+        Self::new(min, max, Vec::new())
     }
 
     pub fn without_zero(min: i32, max: i32) -> IntRange {
+        Self::new(min, max, vec![0])
+    }
+
+    pub fn without_ones(min: i32, max: i32) -> IntRange {
+        Self::new(min, max, vec![1, -1])
+    }
+
+    pub fn without_ones_and_zero(min: i32, max: i32) -> IntRange {
+        Self::new(min, max, vec![-1, 0, 1])
+    }
+
+    fn new(min: i32, max: i32, wanted_exclusions: Vec<i32>) -> IntRange {
         assert!(min <= max, "min must be smaller than max!");
         IntRange {
             min,
             max,
-            include_zero: false,
+            exclude: Self::get_exclusions_in_range(wanted_exclusions, &min, &max),
         }
     }
 
+    /// Makes sure the integers in `exclude` are part of the range
+    ///
+    /// This is important since the number of exclusions is part of the `len` calculation. If there
+    /// are unnecessary exclusions, `len` gets the wrong number.
+    fn get_exclusions_in_range(wanted_exclusions: Vec<i32>, min: &i32, max: &i32) -> Vec<i32> {
+        wanted_exclusions
+            .into_iter()
+            .filter(|num| min <= num && max >= num)
+            .collect()
+    }
+
     pub fn len(&self) -> usize {
-        if !self.include_zero && self.min < 0 && self.max > 0 {
-            (self.max - self.min) as usize
-        } else {
-            (self.max + 1 - self.min) as usize
-        }
+        (self.max + 1 - self.min - self.exclude.len() as i32) as usize
     }
 
     pub fn values(&self) -> Vec<i32> {
         (self.min..=self.max)
-            .filter(|&num| self.include_zero || num != 0)
+            .filter(|num| !self.exclude.contains(num))
             .collect()
     }
 
