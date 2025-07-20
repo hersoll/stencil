4dd63912675870d4c0d6fa8773874fb6f21f4df7
Herman <herman.sollerman@gmail.com>
2025-07-20 17:01:56 +0200

    fix: lowered the numbers in equations with denominators

--- app/src/backend/problems/ma1/equations/equations_with_denominators.rs
+++ app/src/backend/problems/ma1/equations/equations_with_denominators.rs
@@ -1,8 +1,7 @@
-use macros::problem;
+use crate::Result;
 use crate::backend::typst_formatting::{self, equation_solution};
 use crate::backend::{IntRange, Problem};
-use crate::{Result};
-
+use macros::problem;
 
 /// x/3 = 4
 #[problem(id = "one_denom_one_variable", difficulty = 0)]
@@ -32,12 +31,12 @@ fn one_denominator_one_variable(id: String, _lang: &str) -> Result<Problem> {
 /// x/5 + x = 12
 #[problem(id = "one_denom_and_unit_variable_integers_positive", difficulty = 2)]
 fn one_denom_and_unit_variable_integers_positive(id: String, _lang: &str) -> Result<Problem> {
-    let (denominator, denominator_range) = IntRange::without_zero(2, 10)?.and_random();
+    let (denominator, denominator_range) = IntRange::without_zero(3, 5)?.and_random();
     // n is the multiple of the denominator + 1, will show up in both the question and answer
-    let n = IntRange::without_zero(1, 5)?.random();
+    let n = IntRange::without_zero(1, 3)?.random();
     let rhs = n * (denominator + 1);
     let final_answer = n * denominator;
-    let unknown = 'x'; 
+    let unknown = 'x';
 
     let question = format!("${unknown}/{denominator} + {unknown} &= {rhs}$");
     let answer = format!("${unknown} = {final_answer}$");
@@ -47,7 +46,7 @@ fn one_denom_and_unit_variable_integers_positive(id: String, _lang: &str) -> Res
         {denom_plus_one}{unknown} &= {rhs_denom} \\ div {denom_plus_one}\\
         {unknown} &= {final_answer} \\",
         denom_plus_one = denominator + 1,
-        rhs_denom = rhs*denominator,
+        rhs_denom = rhs * denominator,
     ));
 
     Ok(Problem {
@@ -62,12 +61,12 @@ fn one_denom_and_unit_variable_integers_positive(id: String, _lang: &str) -> Res
 /// x - x/3 = 8
 #[problem(id = "unit_variable_and_one_denom_integers_positive", difficulty = 2)]
 fn unit_variable_and_one_denom_integers_positive(id: String, _lang: &str) -> Result<Problem> {
-    let (denominator, denominator_range) = IntRange::without_zero(3, 10)?.and_random();
+    let (denominator, denominator_range) = IntRange::without_zero(3, 5)?.and_random();
     // n is the multiple of the denominator - 1, will show up in both the question and answer
-    let n = IntRange::without_zero(1, 5)?.random();
+    let n = IntRange::without_zero(1, 3)?.random();
     let rhs = n * (denominator - 1);
     let final_answer = n * denominator;
-    let unknown = 'x'; 
+    let unknown = 'x';
 
     let question = format!("${unknown} - {unknown}/{denominator} &= {rhs}$");
     let answer = format!("${unknown} = {final_answer}$");
@@ -77,7 +76,7 @@ fn unit_variable_and_one_denom_integers_positive(id: String, _lang: &str) -> Res
         {denom_minus_one}{unknown} &= {rhs_denom} \\ div {denom_minus_one}\\
         {unknown} &= {final_answer} \\",
         denom_minus_one = denominator - 1,
-        rhs_denom = rhs*denominator,
+        rhs_denom = rhs * denominator,
     ));
 
     Ok(Problem {
@@ -91,14 +90,17 @@ fn unit_variable_and_one_denom_integers_positive(id: String, _lang: &str) -> Res
 }
 
 /// x/4 - x = 9
-#[problem(id = "unit_variable_and_one_denom_integers_with_negatives", difficulty = 3)]
+#[problem(
+    id = "unit_variable_and_one_denom_integers_with_negatives",
+    difficulty = 3
+)]
 fn unit_variable_and_one_denom_integers_with_negatives(id: String, _lang: &str) -> Result<Problem> {
-    let (denominator, denominator_range) = IntRange::without_zero(3, 10)?.and_random();
+    let (denominator, denominator_range) = IntRange::without_zero(3, 5)?.and_random();
     // n is a multiple, will show up in both the question and answer
-    let n = IntRange::without_zero(-5, 5)?.random();
-    let rhs = ( 1 - denominator) * n;
+    let n = IntRange::without_zero(-3, 3)?.random();
+    let rhs = (1 - denominator) * n;
     let final_answer = denominator * n;
-    let unknown = 'x'; 
+    let unknown = 'x';
 
     let question = format!("${unknown}/{denominator} - {unknown} &= {rhs}$");
     let answer = format!("${unknown} = {final_answer}$");
@@ -108,8 +110,8 @@ fn unit_variable_and_one_denom_integers_with_negatives(id: String, _lang: &str) 
         {one_minus_denom}{unknown} &= {rhs_denom} \\ div {one_minus_denom_par}\\
         {unknown} &= {final_answer} \\",
         one_minus_denom = 1 - denominator,
-        one_minus_denom_par = typst_formatting::parentheses(1-denominator),
-        rhs_denom = rhs*denominator,
+        one_minus_denom_par = typst_formatting::parentheses(1 - denominator),
+        rhs_denom = rhs * denominator,
     ));
 
     Ok(Problem {
@@ -119,5 +121,5 @@ fn unit_variable_and_one_denom_integers_with_negatives(id: String, _lang: &str) 
         id,
         identifiers: vec![denominator],
         combinations: denominator_range.len(),
-    }) 
+    })
 }
