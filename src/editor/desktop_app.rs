a74c156adff4dfdb3514467771fa66c76ac6c1bc
Herman <herman.sollerman@gmail.com>
2025-07-28 21:37:06 +0200

    removed dioxus elements

--- src/editor/desktop_app.rs
+++ src/editor/desktop_app.rs
@@ -1,20 +1,12 @@
 use dioxus::prelude::*;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
 
 #[component]
 pub fn DesktopApp() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Link { rel: "preconnect", href: "https://fonts.googleapis.com" }
-        document::Link { rel: "preconnect", href: "https://fonts.gstatic.com" }
-        document::Link {
-            rel: "stylesheet",
-            href: "https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap",
-        }
 
-        p { "Hello to the desktop! This is cool!" }
+        h1 { "Hello to the desktop! This is cool!" }
     }
 }
