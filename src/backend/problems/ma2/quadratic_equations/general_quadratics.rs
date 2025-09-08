ba20ef6f003d2accd8e35b37d97db8c1361e61a4
Herman <herman.sollerman@gmail.com>
2025-09-08 20:46:40 +0200

    feat: moved pq to template

--- src/backend/problems/ma2/quadratic_equations/general_quadratics.rs
+++ src/backend/problems/ma2/quadratic_equations/general_quadratics.rs
@@ -1,31 +1,21 @@
-use macros::problem;
-use crate::backend::{typst_formatting, IntRange, Problem};
 use crate::Result;
+use crate::backend::{IntRange, Problem, solutions};
+use macros::problem;
 
 #[problem]
 fn small_numbers_positive_p(id: String, _lang: &str) -> Result<Problem> {
     let (symmetry, sym_range) = IntRange::without_zero(-3, -1)?.and_random();
     let (distance, dist_range) = IntRange::without_zero(-symmetry + 1, 5)?.and_random();
-    let x_1 = symmetry + distance;
-    let x_2 = symmetry - distance;
-    let p = -2*symmetry;
+    let x_1 = symmetry - distance;
+    let x_2 = symmetry + distance;
+    let p = -2 * symmetry;
     let q = x_1 * x_2;
     let question = format!("$x^2{p:+}x{q:+}=0$");
     let answer = format!("$x_1={x_1}, #h(0.4em) x_2={x_2}$");
     let solution = format!(
-    "$ &x^2{p:+}x{q:+}=0 \\ 
-    &x=-{p}/2 plus.minus sqrt(({p}/2)^2 - {q_par:+}) \\
-    &x={symmetry} plus.minus sqrt({p_d}^2 {q_m:+}) \\
-        &x = {symmetry} plus.minus sqrt({p_sq} {q_m:+}) \\
-        &x = {symmetry} plus.minus sqrt({total_sq}) \\
-        &x = {symmetry} plus.minus {distance} \\
-        &x_1 = {x_1}, #h(0.4em) x_2 = {x_2} $",
-        p_d = p/2,
-        q_par = typst_formatting::parentheses(q),
-        q_m = -q,
-        p_sq = (p/2).pow(2),
-        total_sq = (p/2).pow(2) - q,
-        );
+        "$ {sol} $",
+        sol = solutions::quadratics::pq_short(p, q, 'x')
+    );
 
     Ok(Problem {
         id,
--- src/backend/problems/solutions.rs
+++ src/backend/problems/solutions.rs
@@ -1,2 +1,3 @@
 /// Contains common step-by-step solutions, like linear equations or quadratic equations
 pub mod linear_equations;
+pub mod quadratics;
