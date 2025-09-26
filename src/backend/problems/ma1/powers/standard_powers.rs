ba57583ebe6491e7aee057381f3590bd11646e31
Herman Sollerman (MacBook) <herman.sollerman@gmail.com>
2025-09-26 10:28:17 +0200

    feat: new problem(rust-analyzer fucking up on macbook)

--- src/backend/problems/ma1/powers/standard_powers.rs
+++ src/backend/problems/ma1/powers/standard_powers.rs
@@ -72,3 +72,29 @@ fn double_exponentiation(id: String, _lang: &str) -> Result<Problem> {
         combinations: exp1_range.len() * base_range.len(),
     })
 }
+
+// (5^3 * 5^6) / 5^2
+// Difficulty: 2
+#[problem]
+fn multiplication_and_division(id: String, _lang: &str) -> Result<Problem> {
+    let (base, base_range) = IntRange::without_zero(3, 9)?.and_random();
+    let (exp1, exp1_range) = IntRange::without_zero(2, 10)?.and_random();
+    let (exp2, exp2_range) = IntRange::without_zero(2, 10)?.and_random();
+    let exp3 = IntRange::without_zero(2, exp1 + exp2 - 1)?.random();
+    let total_exp = exp1 + exp2 - exp3;
+    let question = format!("$({base}^{exp1} dot {base}^{exp2})/{base}^{exp3}$");
+    let answer = if total_exp <= 2 {format!("${}$", base.pow(total_exp))} else {format!("${base}^{total_exp}$")};
+    let eval_string = if total_exp <= 2 {format!("= {}", base.pow(total_exp))} else {String::new()};
+    let solution =
+        format!("$ ({base}^{exp1})^{exp2} = {base}^({exp1} dot {exp2}) = {base}^{total_exp} {eval_string} $");
+
+    Ok(Problem {
+        id,
+        question,
+        answer,
+        solution,
+        identifiers: vec![base, exp1, exp2],
+        combinations: exp1_range.len() * base_range.len() * exp2_range.len(),
+    })
+}
+
