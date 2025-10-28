43ebfc96476e2f0e018515489b9638c280cfb056
Herman Sollerman (MacBook) <herman.sollerman@gmail.com>
2025-10-28 15:28:07 +0100

    feat: new problems - powers with variables

--- src/backend/problems/ma1/powers/standard_powers.rs
+++ src/backend/problems/ma1/powers/standard_powers.rs
@@ -1,9 +1,10 @@
 use crate::Result;
+use crate::backend::problems::symbols;
 use crate::backend::{IntRange, Problem};
 use macros::problem;
 
 /// 5^4 * 5^2
-/// Difficulty: 1
+/// Difficulty: 0
 #[problem]
 fn simple_multiplication(id: String, _lang: &str) -> Result<Problem> {
     let base = IntRange::without_zero(4, 9)?.random();
@@ -26,8 +27,32 @@ fn simple_multiplication(id: String, _lang: &str) -> Result<Problem> {
     })
 }
 
-/// 5^4 / 5^2
+/// x^4 * x^2
 /// Difficulty: 1
+#[problem]
+fn simple_multiplication_variables(id: String, _lang: &str) -> Result<Problem> {
+    let base = symbols::get_unknown()?;
+    let (exp1, exp1_range) = IntRange::without_zero(2, 6)?.and_random();
+    let (exp2, exp2_range) = IntRange::without_zero(2, 6)?.and_random();
+    let total_exp = exp1 + exp2;
+    let question = format!("${base}^{exp1} dot {base}^{exp2}$");
+    let answer = format!("${base}^{total_exp}$");
+    let solution = format!(
+        "${base}^{exp1} dot {base}^{exp2} = {base}^({exp1} + {exp2}) = {base}^{total_exp}$"
+    );
+
+    Ok(Problem {
+        id,
+        question,
+        answer,
+        solution,
+        identifiers: vec![exp1, exp2],
+        combinations: exp1_range.len() * exp2_range.len(),
+    })
+}
+
+/// 5^4 / 5^2
+/// Difficulty: 0
 #[problem]
 fn simple_division(id: String, _lang: &str) -> Result<Problem> {
     let (base, base_range) = IntRange::without_zero(4, 9)?.and_random();
@@ -50,6 +75,30 @@ fn simple_division(id: String, _lang: &str) -> Result<Problem> {
     })
 }
 
+
+/// a^4 / a^2
+/// Difficulty: 1
+#[problem]
+fn simple_division_variables(id: String, _lang: &str) -> Result<Problem> {
+    let base = symbols::get_unknown()?;
+    let (exp1, exp1_range) = IntRange::without_zero(4, 10)?.and_random();
+    let exp2 = IntRange::without_zero(2, exp1 - 2)?.random();
+    let total_exp = exp1 - exp2;
+    let question = format!("$display({base}^{exp1} / {base}^{exp2})$");
+    let answer = format!("${base}^{total_exp}$");
+    let solution = format!(
+        "$display({base}^{exp1} / {base}^{exp2} = {base}^({exp1} - {exp2}) = {base}^{total_exp})$"
+    );
+
+    Ok(Problem {
+        id,
+        question,
+        answer,
+        solution,
+        identifiers: vec![exp1],
+        combinations: exp1_range.len(),
+    })
+}
 /// (5^4)^2
 /// Difficulty: 1
 #[problem]
@@ -73,6 +122,29 @@ fn double_exponentiation(id: String, _lang: &str) -> Result<Problem> {
     })
 }
 
+/// (x^4)^2
+/// Difficulty: 1
+#[problem]
+fn double_exponentiation_variables(id: String, _lang: &str) -> Result<Problem> {
+    let base = symbols::get_unknown()?;
+    let (exp1, exp1_range) = IntRange::without_zero(2, 6)?.and_random();
+    let exp2 = IntRange::without_zero(3, 6)?.random();
+    let total_exp = exp1 * exp2;
+    let question = format!("$({base}^{exp1})^{exp2}$");
+    let answer = format!("${base}^{total_exp}$");
+    let solution =
+        format!("$ ({base}^{exp1})^{exp2} = {base}^({exp1} dot {exp2}) = {base}^{total_exp} $");
+
+    Ok(Problem {
+        id,
+        question,
+        answer,
+        solution,
+        identifiers: vec![exp1],
+        combinations: exp1_range.len(),
+    })
+}
+
 // (5^3 * 5^6) / 5^2
 // Difficulty: 2
 #[problem]
@@ -80,13 +152,13 @@ fn multiplication_and_division(id: String, _lang: &str) -> Result<Problem> {
     let (base, base_range) = IntRange::without_zero(3, 9)?.and_random();
     let (exp1, exp1_range) = IntRange::without_zero(2, 10)?.and_random();
     let (exp2, exp2_range) = IntRange::without_zero(2, 10)?.and_random();
-    let exp3 = IntRange::without_zero(2, exp1 + exp2 - 1)?.random();
+    let exp3 = IntRange::without_zero(2, exp1 + exp2 - 2)?.exclude(exp1).exclude(exp2).random();
     let total_exp = exp1 + exp2 - exp3;
-    let question = format!("$({base}^{exp1} dot {base}^{exp2})/{base}^{exp3}$");
-    let answer = if total_exp <= 2 {format!("${}$", base.pow(total_exp))} else {format!("${base}^{total_exp}$")};
-    let eval_string = if total_exp <= 2 {format!("= {}", base.pow(total_exp))} else {String::new()};
+    let question = format!("$ ({base}^{exp1} dot {base}^{exp2})/{base}^{exp3} $");
+    let answer = format!("${base}^{total_exp}$");
     let solution =
-        format!("$ ({base}^{exp1})^{exp2} = {base}^({exp1} dot {exp2}) = {base}^{total_exp} {eval_string} $");
+        format!("$ ({base}^{exp1} dot {base}^{exp2})/{base}^{exp3} = {base}^{}/{base}^{exp3} = {base}^{total_exp} $",
+        exp1 + exp2);
 
     Ok(Problem {
         id,
