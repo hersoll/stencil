26c3fa989e7bbb7e10e2bd6bbb688d304db7e8db
Herman <herman.sollerman@gmail.com>
2025-12-07 14:03:45 +0100

    feat: added baseline tracing functionality

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -4,16 +4,13 @@ mod problems;
 pub use i18n::I18nDatabase;
 pub use problems::ProblemDatabase;
 
-use dotenvy;
 use once_cell::sync::OnceCell;
 use sqlx::{PgPool, postgres::PgPoolOptions};
 
 static DB_POOL: OnceCell<PgPool> = OnceCell::new();
 
 pub async fn init_database() -> Result<(), Box<dyn std::error::Error>> {
-    dotenvy::dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    println!("Connecting to database...");
     let pool = PgPoolOptions::new()
         .max_connections(20)
         .after_connect(|conn, _meta| {
@@ -36,7 +33,6 @@ pub async fn init_database() -> Result<(), Box<dyn std::error::Error>> {
     // Test the connection
     let pool = get_pool();
     let _ = sqlx::query("SELECT 1").execute(pool).await?;
-    println!("Database connected successfully!");
 
     Ok(())
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,58 +1,130 @@
+use std::time::Duration;
+
 use stencil::pdf_generation;
-use tower_http::cors::{Any, CorsLayer};
+use tower_http::{
+    cors::{Any, CorsLayer},
+    trace::TraceLayer,
+};
 use tracing_subscriber;
 
-use axum::{Router, http::Method, routing::get};
+use axum::{
+    Router,
+    http::{Method, Request, StatusCode},
+    routing::get,
+};
+use tracing::{error, info, warn};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
-    let host = std::env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
-    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+
+    // Set up tracing
+    let default_filter = if cfg!(debug_assertions) {
+        "debug,stencil=debug,sqlx=warn"
+    } else {
+        "info,stencil=info,sqlx=warn"
+    };
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| default_filter.into()),
+        )
+        .with(tracing_subscriber::fmt::layer().with_target(true))
+        .init();
+
+    info!("Starting server...");
+
+    let default_host = "127.0.0.1";
+    let default_port = "3000";
+    let host = std::env::var("HOST").unwrap_or_else(|_| {
+        warn!("HOST not found in .env, using {default_host}.");
+        default_host.to_string()
+    });
+    let port = std::env::var("PORT").unwrap_or_else(|_| {
+        warn!("PORT not found in .env, using {default_port}.");
+        default_port.to_string()
+    });
     let addr = format!("{}:{}", host, port);
+
+    info!("Initializing db...");
+    match stencil::db::init_database().await {
+        Ok(_) => info!("Finished initializing db!"),
+        Err(_) => error!("Failed to initialize db!"),
+    }
+
+    info!("Loading problems to registry...");
+    match stencil::load_problem_data().await {
+        Ok(_) => info!("Problems loaded!"),
+        Err(_) => error!("Failed to load problems from registry!"),
+    }
+    info!("Loading prefixes to registry...");
+    match stencil::load_prefix_data().await {
+        Ok(_) => info!("Prefixes loaded!"),
+        Err(_) => error!("Failed to load prefixes from registry!"),
+    }
 
     #[cfg(feature = "docker")]
     let frontend_port = "5172";
     #[cfg(not(feature = "docker"))]
     let frontend_port = "5173";
 
+    let frontend_url = format!("http://localhost:{frontend_port}");
+
     let cors = CorsLayer::new()
-        .allow_origin(
-            ("http://localhost:".to_string() + frontend_port)
-                .parse::<axum::http::HeaderValue>()
-                .unwrap(),
-        )
+        .allow_origin(frontend_url.parse::<axum::http::HeaderValue>().unwrap())
         .allow_methods([Method::GET, Method::POST])
         .allow_headers(Any);
+    info!("Allowing connections from {frontend_url}.");
 
-    tracing_subscriber::fmt::init();
-
-    stencil::db::init_database()
-        .await
-        .expect("Failed to initialize db");
-
-    println!("Loading problems to registry...");
-    stencil::load_problem_data()
-        .await
-        .expect("Failed to load problems");
-    println!("Problems loaded!");
-    println!("Loading prefixes to registry...");
-    stencil::load_prefix_data()
-        .await
-        .expect("Failed to load prefixes");
-    println!("Prefixes loaded!");
-
-    let app = Router::new()
-        .route("/", get(hello_world))
-        .route("/pdf", get(pdf_generation::send_pdf))
-        .layer(cors);
+    let app = create_router(cors);
 
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    println!("\nListening on port {port}...");
+    println!();
+    info!("Listening on {}...", listener.local_addr().unwrap());
 
     axum::serve(listener, app).await.unwrap();
 }
 
+fn create_router(cors: CorsLayer) -> Router {
+    Router::new()
+        .route("/", get(hello_world))
+        .route("/error", get(error))
+        .route("/pdf", get(pdf_generation::send_pdf))
+        .layer(cors)
+        // Add tracing middleware
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    tracing::info_span!(
+                        "http",
+                        method = ?request.method(),
+                        uri = ?request.uri(),
+                    )
+                })
+                .on_response(
+                    |response: &axum::response::Response,
+                     latency: Duration,
+                     _span: &tracing::Span| {
+                        tracing::info!(
+                            status = response.status().as_u16(),
+                            latency = ?latency,
+                            "response"
+                        );
+                    },
+                ),
+        )
+}
+
 async fn hello_world() -> String {
-    String::from("Hello, world!")
+    String::from(
+        "Hello from the \"/\" path! Did you mean to hit the \"/api\"?
+            Try hitting the \"/error\" path to get a funny HTTP error!
+        ",
+    )
+}
+
+async fn error() -> Result<String, StatusCode> {
+    error!("This is an error code!");
+    Err(StatusCode::UNAVAILABLE_FOR_LEGAL_REASONS)
 }
