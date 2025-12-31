800a8339f0b4db626a5b2bbebfb6e684cf60b437
Herman <herman.sollerman@gmail.com>
2025-12-31 19:24:56 +0100

    feat: added some text to the graph example

--- backend/src/problems/ma1/straight_lines/testing.rs
+++ backend/src/problems/ma1/straight_lines/testing.rs
@@ -7,7 +7,7 @@ use anyhow::Result;
 /// Difficulty: 0
 #[problem]
 fn test_graph(name: String, _lang: &Language) -> Result<Problem> {
-    let question = math::graphing::Graph::new().render()?;
+    let question = String::from("Här är en graf\n") + &math::graphing::Graph::new().render()?;
     let answer = String::from("$2x + 1$");
     let solution = String::from("Reading the line explains the line");
 
