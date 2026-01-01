cb3966390363b53f1ad76949bf889745f1d9ac50
Herman Sollerman (MacBook) <herman.sollerman@gmail.com>
2026-01-01 20:59:22 +0100

    feat: added plotting capabilites to the graph

--- backend/src/math/graphing.rs
+++ backend/src/math/graphing.rs
@@ -1,5 +1,5 @@
-use anyhow::Result;
-use std::fmt::Write;
+use anyhow::{anyhow, Result};
+use std::{fmt::Write, i32};
 
 pub struct Graph {
     x_min: i32,
@@ -10,6 +10,9 @@ pub struct Graph {
     y_tick: i32,
     x_grid: GridType,
     y_grid: GridType,
+    /// Can the graph start at a non-zero value and "break" to the x-axis?
+    can_break: bool,
+    plots: Vec<PlotType>,
 }
 
 impl Default for Graph {
@@ -23,6 +26,8 @@ impl Default for Graph {
             y_tick: 1,
             x_grid: GridType::Major,
             y_grid: GridType::Major,
+            can_break: false,
+            plots: Vec::new(),
         }
     }
 }
@@ -33,14 +38,97 @@ enum GridType {
     None,
 }
 
+impl GridType {
+    fn to_typst(&self) -> &str {
+        match self {
+            GridType::Both => "both",
+            GridType::Major => "major",
+            GridType::None => "none",
+        }
+    }
+}
+
+// TODO: Replace with Number
+pub enum PlotType {
+    /// k, m
+    Linear(i32, i32),
+    Polynomial(Vec<i32>),
+    /// start, change
+    Exponential(i32, i32),
+}
+
+impl PlotType {
+    fn to_typst(&self) -> String {
+        match self {
+            PlotType::Linear(k, m) => format!("{} * x + {}", k, m),
+            PlotType::Exponential(start, change) => format!("{} * calc.pow({}, x)", start, change),
+            // TODO:
+            PlotType::Polynomial(_) => format!("3"),
+        }
+    }
+}
+
 impl Graph {
     pub fn new() -> Self {
         Graph::default()
     }
 
-    pub fn render(&self) -> Result<String> {
+    pub fn x_range(&mut self, min: i32, max: i32) -> &mut Self {
+        self.x_min = min;
+        self.x_max = max;
+        self
+    }
+
+    pub fn auto_y_range(&mut self) -> Result<&mut Self> {
+        if self.plots.len() == 0 {
+            return Err(anyhow!(
+                "Tried to call auto_y_range() without adding a plot first",
+            ));
+        }
+
+        let mut min = i32::MAX;
+        let mut max = i32::MIN;
+        //TODO: handle polynomials properly
+        for plot in self.plots.iter() {
+            for val in [self.x_min, self.x_max] {
+                let extreme = match plot {
+                    PlotType::Linear(k, m) => k * val + m,
+                    PlotType::Exponential(c, a) => c * a.pow(val as u32),
+                    PlotType::Polynomial(_) => 0,
+                };
+
+                if extreme < min {
+                    min = extreme;
+                }
+                if extreme > max {
+                    max = extreme;
+                }
+            }
+        }
+
+        self.y_min = min;
+        self.y_max = max;
+
+        Ok(self)
+    }
+
+    pub fn y_range(&mut self, min: i32, max: i32) -> &mut Self {
+        self.y_min = min;
+        self.y_max = max;
+        self
+    }
+
+    pub fn add_plot(&mut self, plot: PlotType) -> &mut Self {
+        self.plots.push(plot);
+        self
+    }
+
+    pub fn render(&mut self) -> Result<String> {
+        if !self.can_break {
+            self.move_y_range_to_axis();
+        }
         let mut out = String::with_capacity(256);
-        write!(out, "#block(height: 5cm)[#cetz.canvas({{")?;
+        write!(out, "#block(height: 5cm)[#cetz.canvas({{\n")?;
         write!(out, "plot.plot(")?;
         write!(out, "axis-style: \"school-book\",")?;
         write!(out, "size: (4, 4),")?;
@@ -48,10 +136,32 @@ impl Graph {
         write!(out, "x-max: {},", self.x_max)?;
         write!(out, "y-min: {},", self.y_min)?;
         write!(out, "y-max: {},", self.y_max)?;
+        write!(out, "x-grid: \"{}\",", self.x_grid.to_typst())?;
+        write!(out, "y-grid: \"{}\",", self.y_grid.to_typst())?;
         write!(out, "x-tick-step: {},", self.x_tick)?;
-        write!(out, "y-tick-step: {},", self.y_tick)?;
-        write!(out, "{{ plot.add(domain: (-2, 2), x => 2 * x + 1) }},")?;
+        write!(out, "y-tick-step: {},\n", self.y_tick)?;
+        for plot in self.plots.iter() {
+            write!(
+                out,
+                "plot.add(domain: ({}, {}), x => {}),",
+                self.x_min,
+                self.x_max,
+                plot.to_typst()
+            )?;
+        }
         write!(out, ")}})]")?;
         Ok(out)
+    }
+
+    /// Moves the y_range to make sure the graph goes to 0.
+    /// Not used if the graph has can_break: true.
+    fn move_y_range_to_axis(&mut self) {
+        if self.y_min < 0 && self.y_max < 0 {
+            self.y_max = 0
+        };
+
+        if self.y_min > 0 && self.y_max > 0 {
+            self.y_min = 0
+        };
     }
 }
--- backend/src/problems/ma1/straight_lines/testing.rs
+++ backend/src/problems/ma1/straight_lines/testing.rs
@@ -1,22 +1,64 @@
 use macros::problem;
 
-use crate::{Language, math, problems::Problem};
+use crate::{
+    math::{self, graphing::PlotType, IntRange},
+    problems::Problem,
+    Language,
+};
 use anyhow::Result;
 
-/// Testing
-/// Difficulty: 0
+/// Test linear graph
+/// Difficulty: 2
 #[problem]
-fn test_graph(name: String, _lang: &Language) -> Result<Problem> {
-    let question = String::from("Här är en graf\n") + &math::graphing::Graph::new().render()?;
-    let answer = String::from("$2x + 1$");
-    let solution = String::from("Reading the line explains the line");
+fn linear_graph(name: String, _lang: &Language) -> Result<Problem> {
+    let (k, k_range) = IntRange::with_zero(-3, 3)?.and_random();
+    let (m, m_range) = IntRange::with_zero(-3, 3)?.and_random();
+    let x_min = -1;
+    let x_max = 2;
+    let graph = math::graphing::Graph::new()
+        .x_range(x_min, x_max)
+        .add_plot(PlotType::Linear(k, m))
+        .auto_y_range()?
+        .render()?;
+
+    let question = format!("Här är en rät linje: \n {graph}");
+    let answer = format!("$y = {k}x {m:+}$");
+    let solution = String::from("look at the fucking line");
 
     Ok(Problem {
         name,
         question,
         answer,
         solution,
-        identifiers: vec![1],
-        combinations: 1,
+        identifiers: vec![k, m],
+        combinations: k_range.len() * m_range.len(),
+    })
+}
+
+/// Test exponential graph
+/// Difficulty: 5
+#[problem]
+fn exponential_graph(name: String, _lang: &Language) -> Result<Problem> {
+    let (c, c_range) = IntRange::with_zero(1, 3)?.and_random();
+    let (a, a_range) = IntRange::with_zero(2, 3)?.and_random();
+    let x_min = 0;
+    let x_max = 2;
+    let graph = math::graphing::Graph::new()
+        .x_range(x_min as i32, x_max as i32)
+        .add_plot(PlotType::Exponential(c, a))
+        .auto_y_range()?
+        .render()?;
+
+    let question = format!("Här är en exponentialfunktion: \n {graph}");
+    let answer = format!("$y = {c} dot {a}^x$");
+    let solution = String::from("look at the fucking line");
+
+    Ok(Problem {
+        name,
+        question,
+        answer,
+        solution,
+        identifiers: vec![c, a],
+        combinations: c_range.len() * a_range.len(),
     })
 }
