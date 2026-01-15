a0b7cae48982ef501faf94f8de1fa98b48d91979
Herman <herman.sollerman@gmail.com>
2026-01-15 16:43:06 +0100

    test(functions): added linear function tests

--- backend/src/math/functions.rs
+++ backend/src/math/functions.rs
@@ -13,9 +13,13 @@ pub enum Function {
 impl Function {
     pub fn get_x(&self, y: &Number) -> Option<Number> {
         match self {
-            Function::Linear(k, m) => Some((y - m) / k),
+            Function::Linear(k, m) => {
+                // Don't use this when k = 0, please
+                if *k == ZERO { None } else { Some((y - m) / k) }
+            }
             Function::Exponential(c, a) => {
-                // No solution if y and c have opposite signs
+                // No solution if y and c have opposite signs,
+                // since a positive value can't become negative through exponentiation
                 if y * c < ZERO {
                     None
                 } else {
@@ -35,18 +39,112 @@ impl Function {
 }
 
 #[cfg(test)]
-mod tests {
+mod linear_function_tests {
     use super::*;
     use crate::typst_utils::graphing::Graph;
 
-    #[test]
-    fn linear_function_integers_get_y() {
-        let f = Graph::linear(1, 2);
-        let g = Graph::linear(0, 2);
-        let h = Graph::linear(-3, -5);
-        let x = Number::Integer(-1);
-        assert_eq!(f.function.get_y(&x), Number::Integer(1));
-        assert_eq!(g.function.get_y(&x), Number::Integer(2));
-        assert_eq!(h.function.get_y(&x), Number::Integer(-2));
+    mod get_y {
+        use super::*;
+
+        #[test]
+        fn integers() {
+            let x = Number::Integer(-1);
+            let cases = [
+                (Graph::linear(1, 2), Number::Integer(1)),
+                (Graph::linear(0, 2), Number::Integer(2)),
+                (Graph::linear(-3, -5), Number::Integer(-2)),
+            ];
+
+            for (graph, expected) in cases {
+                assert_eq!(graph.function.get_y(&x), expected);
+            }
+        }
+
+        #[test]
+        fn decimals() {
+            let x = Number::Decimal(2500); // 2.5
+
+            let cases = [
+                (Graph::linear(1.1, 1.1), Number::Decimal(3850)),
+                (Graph::linear(0, -3.7), Number::Decimal(-3700)),
+                (Graph::linear(1.234, 5), Number::Decimal(8085)),
+            ];
+
+            for (graph, expected) in cases {
+                assert_eq!(graph.function.get_y(&x), expected);
+            }
+        }
+
+        #[test]
+        fn fractions() {
+            let x = Number::Fraction(2, 5);
+
+            let cases = [
+                (Graph::linear((3, 5), 1), Number::Fraction(31, 25)),
+                (Graph::linear(0, (2, 3)), Number::Fraction(2, 3)),
+                (Graph::linear((-4, 3), (1, 6)), Number::Fraction(-11, 30)),
+            ];
+
+            for (graph, expected) in cases {
+                assert_eq!(graph.function.get_y(&x), expected);
+            }
+        }
+    }
+
+    mod get_x {
+        use super::*;
+
+        #[test]
+        fn integers() {
+            let y = Number::Integer(4);
+
+            let cases = [
+                (Graph::linear(3, 2), Some(Number::Fraction(2, 3))),
+                (Graph::linear(0, 2), None),
+                (Graph::linear(-1, -5), Some(Number::Integer(-9))),
+            ];
+
+            for (graph, expected) in cases {
+                assert_eq!(graph.function.get_x(&y), expected);
+            }
+        }
+
+        #[test]
+        fn decimals() {
+            let y = Number::Decimal(2500); // 2.5
+
+            let cases = [
+                (Graph::linear(1.1, 1.1), Some(Number::Decimal(1273))),
+                (Graph::linear(0, -3.7), None),
+                (Graph::linear(1.234, 5), Some(Number::Decimal(-2026))),
+            ];
+
+            for (graph, expected) in cases {
+                assert_eq!(graph.function.get_x(&y), expected);
+            }
+        }
+
+        #[test]
+        fn fractions() {
+            let y = Number::Fraction(2, 5);
+
+            let cases = [
+                (Graph::linear((3, 5), 1), Some(Number::Integer(-1))),
+                (Graph::linear(0, (2, 3)), None),
+                (
+                    Graph::linear((-4, 3), (1, 6)),
+                    Some(Number::Fraction(-21, 120)),
+                ),
+                // Test another fraction representation
+                (
+                    Graph::linear((-4, 3), (1, 6)),
+                    Some(Number::Fraction(7, -40)),
+                ),
+            ];
+
+            for (graph, expected) in cases {
+                assert_eq!(graph.function.get_x(&y), expected);
+            }
+        }
     }
 }
