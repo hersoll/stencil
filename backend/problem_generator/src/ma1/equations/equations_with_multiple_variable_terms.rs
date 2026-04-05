0afb6d5976f5b31862114a1d0ea7838ce668143e
Herman Sollerman (MacBook) <herman.sollerman@gmail.com>
2026-04-05 22:31:10 +0200

    problem: equations_with_multiple_variable_terms, rhs greater

--- backend/problem_generator/src/ma1/equations/equations_with_multiple_variable_terms.rs
+++ backend/problem_generator/src/ma1/equations/equations_with_multiple_variable_terms.rs
@@ -4,7 +4,7 @@ use math::{Term, num_gen, symbols};
 use types::{lang::Language, problems::Problem};
 use typst_writer::formatting::{self, equation_solution};
 
-/// 4x + 1 = 2x - 3
+/// 4x + 1 = 2x + 3
 /// Difficulty: 1
 #[problem]
 fn two_positive_coefs_lhs_greater(name: String, _lang: &Language) -> Result<Problem> {
@@ -39,5 +39,43 @@ fn two_positive_coefs_lhs_greater(name: String, _lang: &Language) -> Result<Prob
         solution,
         identifiers: vec![lhs_coef, rhs_coef],
         combinations: lhs_range.len(),
+    })
+}
+
+/// 2x + 1 = 4x + 3
+/// Difficulty: 1
+#[problem]
+fn two_positive_coefs_rhs_greater(name: String, _lang: &Language) -> Result<Problem> {
+    let unknown = symbols::get_unknown()?;
+    let answer = num_gen::integer().range(1, 6).random();
+    let rhs_range = num_gen::integer().range(3, 9);
+    let rhs_coef = rhs_range.random();
+    let rhs_term = Term::from_num_and_vars(rhs_coef, unknown);
+    let lhs_coef = num_gen::integer().range(1, rhs_coef - 2).random();
+    let lhs_term = Term::from_num_and_vars(lhs_coef, unknown);
+    let rhs_const = num_gen::integer().range(1, 9).random();
+    let lhs_const = (rhs_coef - lhs_coef) * answer + rhs_const;
+
+    let question = format!("${lhs_term}{lhs_const:+} = {rhs_term}{rhs_const:+}$");
+    let answer_str = format!("${unknown} = {answer}$");
+    let solution = equation_solution(format!(
+        "{lhs_term}{lhs_const:+} &= {rhs_term}{rhs_const:+} \\ {sub_lhs} \\
+        {lhs_const} &= {total_var}{rhs_const:+} \\ {sub_rhs} \\
+        {total_const} &= {total_var} \\ {div_coef} \\
+        {answer} &= {unknown} \\ \\",
+        sub_lhs = formatting::subtract_term(&lhs_term),
+        sub_rhs = formatting::subtract_number(rhs_const),
+        total_var = rhs_term.clone() - lhs_term.clone(),
+        div_coef = formatting::divide_number(rhs_coef - lhs_coef),
+        total_const = lhs_const - rhs_const
+    ));
+
+    Ok(Problem {
+        name,
+        question,
+        answer: answer_str,
+        solution,
+        identifiers: vec![lhs_coef, rhs_coef],
+        combinations: rhs_range.len(),
     })
 }
