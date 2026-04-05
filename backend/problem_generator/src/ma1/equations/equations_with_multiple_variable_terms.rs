93b932b4205ee07edb6441ab673b96dc77be1f65
Herman Sollerman (MacBook) <herman.sollerman@gmail.com>
2026-04-05 22:21:47 +0200

    refactor(problem): rewrote some of equations_with_multiple_variable_terms with new syntax

--- backend/problem_generator/src/ma1/equations/equations_with_multiple_variable_terms.rs
+++ backend/problem_generator/src/ma1/equations/equations_with_multiple_variable_terms.rs
@@ -1,33 +1,34 @@
 use anyhow::Result;
 use macros::problem;
-use math::{IntRange, Term, symbols};
+use math::{Term, num_gen, symbols};
 use types::{lang::Language, problems::Problem};
-use typst_writer::formatting::equation_solution;
+use typst_writer::formatting::{self, equation_solution};
 
 /// 4x + 1 = 2x - 3
 /// Difficulty: 1
 #[problem]
 fn two_positive_coefs_lhs_greater(name: String, _lang: &Language) -> Result<Problem> {
     let unknown = symbols::get_unknown()?;
-    let answer = IntRange::without_zero(1, 6)?.random();
-    let (lhs_coef, lhs_range) = IntRange::without_zero(3, 9)?.and_random();
-    let lhs_var: Term = (lhs_coef, unknown).into();
-    let rhs_coef = IntRange::without_zero(1, lhs_coef - 2)?.random();
-    let rhs_var: Term = (rhs_coef, unknown).into();
-    let lhs_const = IntRange::without_zero(1, 9)?.random();
+    let answer = num_gen::integer().range(1, 6).random();
+    let lhs_range = num_gen::integer().range(3, 9);
+    let lhs_coef = lhs_range.random();
+    let lhs_term = Term::from_num_and_vars(lhs_coef, unknown);
+    let rhs_coef = num_gen::integer().range(1, lhs_coef - 2).random();
+    let rhs_term = Term::from_num_and_vars(rhs_coef, unknown);
+    let lhs_const = num_gen::integer().range(1, 9).random();
     let rhs_const = (lhs_coef - rhs_coef) * answer + lhs_const;
 
-    let question = format!("${lhs_var}{lhs_const:+} = {rhs_var}{rhs_const:+}$");
+    let question = format!("${lhs_term}{lhs_const:+} = {rhs_term}{rhs_const:+}$");
     let answer_str = format!("${unknown} = {answer}$");
     let solution = equation_solution(format!(
-        "{lhs_var}{lhs_const:+} &= {rhs_var}{rhs_const:+} \\ {sub_rhs} \\
+        "{lhs_term}{lhs_const:+} &= {rhs_term}{rhs_const:+} \\ {sub_rhs} \\
         {total_var}{lhs_const:+} &= {rhs_const} \\ {sub_lhs} \\
         {total_var} &= {total_const} \\ {div_coef} \\
         {unknown} &= {answer} \\ \\",
-        sub_rhs = typst_writer::formatting::subtract_term(&rhs_var),
-        sub_lhs = typst_writer::formatting::subtract_number(lhs_const),
-        total_var = lhs_var.clone() - rhs_var.clone(),
-        div_coef = typst_writer::formatting::divide_number(lhs_coef - rhs_coef),
+        sub_rhs = formatting::subtract_term(&rhs_term),
+        sub_lhs = formatting::subtract_number(lhs_const),
+        total_var = lhs_term.clone() - rhs_term.clone(),
+        div_coef = formatting::divide_number(lhs_coef - rhs_coef),
         total_const = rhs_const - lhs_const
     ));
 
