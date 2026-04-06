362ad58e0491682ddcc972d7edd8b7ecd84b8321
Herman <herman.sollerman@gmail.com>
2026-04-06 13:25:27 +0200

    problem: one_negative_coef_lhs_greater

--- backend/problem_generator/src/ma1/equations/equations_with_multiple_variable_terms.rs
+++ backend/problem_generator/src/ma1/equations/equations_with_multiple_variable_terms.rs
@@ -79,3 +79,41 @@ fn two_positive_coefs_rhs_greater(name: String, _lang: &Language) -> Result<Prob
         combinations: rhs_range.len(),
     })
 }
+
+/// 4x + 1 = 4 - 2x
+/// Difficulty: 2
+#[problem]
+fn one_negative_coef_lhs_greater(name: String, _lang: &Language) -> Result<Problem> {
+    let unknown = symbols::get_unknown()?;
+    let answer = num_gen::integer().range(-8, 8).exclude(0).random();
+    let lhs_range = num_gen::integer().range(1, 9);
+    let lhs_coef = lhs_range.random();
+    let lhs_term = Term::from_num_and_vars(lhs_coef, unknown);
+    let rhs_coef = num_gen::integer().range(lhs_coef - 10, -1).random();
+    let rhs_term = Term::from_num_and_vars(rhs_coef, unknown);
+    let rhs_const = num_gen::integer().range(1, 9).random();
+    let lhs_const = (rhs_coef - lhs_coef) * answer + rhs_const;
+
+    let question = format!("${lhs_term}{lhs_const:+} = {rhs_const}{rhs_term:+}$");
+    let answer_str = format!("${unknown} = {answer}$");
+    let solution = equation_solution(format!(
+        "{lhs_term}{lhs_const:+} &= {rhs_const}{rhs_term:+} \\ {sub_rhs} \\
+        {total_var}{lhs_const:+} &= {rhs_const} \\ {sub_lhs} \\
+        {total_var} &= {total_const} \\ {div_coef} \\
+        {unknown} &= {answer} \\ \\",
+        sub_rhs = formatting::subtract_term(&rhs_term),
+        sub_lhs = formatting::subtract_number(lhs_const),
+        total_var = lhs_term.clone() - rhs_term.clone(),
+        div_coef = formatting::divide_number(lhs_coef - rhs_coef),
+        total_const = rhs_const - lhs_const
+    ));
+
+    Ok(Problem {
+        name,
+        question,
+        answer: answer_str,
+        solution,
+        identifiers: vec![lhs_coef, rhs_coef],
+        combinations: lhs_range.len(),
+    })
+}
