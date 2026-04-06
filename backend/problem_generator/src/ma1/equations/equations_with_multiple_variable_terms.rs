081cf1a90abfdf6965724c2571b4105fa9907361
Herman <herman.sollerman@gmail.com>
2026-04-06 13:34:55 +0200

    problem: one_negative_coef_rhs_greater

--- backend/problem_generator/src/ma1/equations/equations_with_multiple_variable_terms.rs
+++ backend/problem_generator/src/ma1/equations/equations_with_multiple_variable_terms.rs
@@ -117,3 +117,41 @@ fn one_negative_coef_lhs_greater(name: String, _lang: &Language) -> Result<Probl
         combinations: lhs_range.len(),
     })
 }
+
+/// 4 - 2x = 1 + 4x
+/// Difficulty: 2
+#[problem]
+fn one_negative_coef_rhs_greater(name: String, _lang: &Language) -> Result<Problem> {
+    let unknown = symbols::get_unknown()?;
+    let answer = num_gen::integer().range(-8, 8).exclude(0).random();
+    let rhs_range = num_gen::integer().range(1, 9);
+    let rhs_coef = rhs_range.random();
+    let rhs_term = Term::from_num_and_vars(rhs_coef, unknown);
+    let lhs_coef = num_gen::integer().range(rhs_coef - 10, -1).random();
+    let lhs_term = Term::from_num_and_vars(lhs_coef, unknown);
+    let lhs_const = num_gen::integer().range(1, 9).random();
+    let rhs_const = (lhs_coef - rhs_coef) * answer + lhs_const;
+
+    let question = format!("${lhs_const}{lhs_term:+} = {rhs_term}{rhs_const:+}$");
+    let answer_str = format!("${unknown} = {answer}$");
+    let solution = equation_solution(format!(
+        "{lhs_const}{lhs_term:+} &= {rhs_term}{rhs_const:+} \\ {sub_lhs} \\
+        {lhs_const} &= {total_var}{rhs_const:+} \\ {sub_rhs} \\
+        {total_const} &= {total_var} \\ {div_coef} \\
+        {answer} &= {unknown} \\ \\",
+        sub_lhs = formatting::subtract_term(&lhs_term),
+        sub_rhs = formatting::subtract_number(rhs_const),
+        total_var = rhs_term.clone() - lhs_term.clone(),
+        div_coef = formatting::divide_number(rhs_coef - lhs_coef),
+        total_const = lhs_const - rhs_const
+    ));
+
+    Ok(Problem {
+        name,
+        question,
+        answer: answer_str,
+        solution,
+        identifiers: vec![lhs_coef, rhs_coef],
+        combinations: rhs_range.len(),
+    })
+}
