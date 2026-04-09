849ba5e8e7ab6c7e71b1c63ff0eeea9dfa0aeb4c
Herman Sollerman (MacBook) <herman.sollerman@gmail.com>
2026-04-09 10:10:10 +0200

    refactor: changed file structure of math crate

--- backend/math/src/lib.rs
+++ backend/math/src/lib.rs
@@ -1,9 +1,16 @@
 pub mod functions;
 mod int_range;
 pub mod num_gen;
+mod numbers;
+mod polynomials;
 pub mod symbols;
-mod types;
+mod terms;
 pub mod utils;
+mod variables;
 
 pub use int_range::IntRange;
-pub use types::*;
+pub use numbers::*;
+pub use polynomials::Polynomial;
+pub use terms::Term;
+pub use variables::Variable;
+pub use variables::Variables;
