15a049b30d0b1e011168f71e853a560edc3d03f1
Herman Sollerman (MacBook) <herman.sollerman@gmail.com>
2026-05-08 15:02:44 +0200

    problems: decimal_form

--- backend/problem_generator/src/ma1/percentages/decimal_form.rs
+++ backend/problem_generator/src/ma1/percentages/decimal_form.rs
@@ -27,9 +27,48 @@ fn two_digit_to_decimal_form(id: i32, _lang: Language) -> Result<Problem> {
     })
 }
 
-// /// Write 145% in decimal form
-// /// Difficulty: 0
-// #[problem]
-// fn three_digit_to_decimal_form(id: i32, _lang: Language) -> Result<Problem> {
-//     todo!()
-// }
+/// Write 156% in decimal form
+/// Difficulty: 1
+#[problem]
+fn three_digit_to_decimal_form(id: i32, _lang: Language) -> Result<Problem> {
+    let percentage_range = num_gen::integer()
+        .range(101, 499)
+        .exclude_multiple(&[200, 300, 400]);
+    let percentage = percentage_range.random();
+    let decimal_form = (percentage / 100).to_decimal();
+
+    let question = format!("${percentage}%$");
+    let answer = format!("${decimal_form}$");
+    let solution = format!("$1% = num(0.01) \\ 10% = num(0.10) \\ {percentage}% = {decimal_form}$");
+
+    Ok(Problem {
+        id,
+        question,
+        answer,
+        solution,
+        identifiers: vec![percentage],
+        combinations: percentage_range.len(),
+    })
+}
+
+/// Convert 0,3 to percent form
+/// Difficulty: 0
+#[problem]
+fn decimal_to_percent(id: i32, _lang: Language) -> Result<Problem> {
+    let percentage_range = num_gen::integer().numbers(&[10, 20, 30, 40, 50, 60, 70, 80, 90]);
+    let percentage = percentage_range.random();
+    let decimal_form = (percentage / 100).to_decimal();
+
+    let question = format!("${decimal_form}$");
+    let answer = format!("${percentage}%$");
+    let solution = format!("${decimal_form} = {decimal_form:.2} = {percentage}%$");
+
+    Ok(Problem {
+        id,
+        question,
+        answer,
+        solution,
+        identifiers: vec![percentage],
+        combinations: percentage_range.len(),
+    })
+}
