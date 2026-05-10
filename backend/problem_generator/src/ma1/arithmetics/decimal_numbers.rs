839d976012ac5e0b5dbacfe78b375f4ae96ca7dc
Herman <herman.sollerman@gmail.com>
2026-05-10 16:54:04 +0200

    problems: decimal_numbers

--- backend/problem_generator/src/ma1/arithmetics/decimal_numbers.rs
+++ backend/problem_generator/src/ma1/arithmetics/decimal_numbers.rs
@@ -6,7 +6,7 @@ use types::{lang::Language, problems::Problem};
 /// 0,14 + 0,3
 /// Difficulty: 0
 #[problem]
-fn add_different_decimals(id: i32, _lang: Language) -> Result<Problem> {
+fn add_decimals_two_decimals_first(id: i32, _lang: Language) -> Result<Problem> {
     let two_decimal_range = num_gen::decimal().with_decimals(2).range(0.1, 0.3);
     let two_decimal_num = two_decimal_range.random();
     let one_decimal_range = num_gen::decimal().with_decimals(1).range(0.2, 0.6);
@@ -27,10 +27,34 @@ fn add_different_decimals(id: i32, _lang: Language) -> Result<Problem> {
     })
 }
 
+/// 0,3 + 0,14
+/// Difficulty: 0
+#[problem]
+fn add_decimals_one_decimal_first(id: i32, _lang: Language) -> Result<Problem> {
+    let two_decimal_range = num_gen::decimal().with_decimals(2).range(0.1, 0.3);
+    let two_decimal_num = two_decimal_range.random();
+    let one_decimal_range = num_gen::decimal().with_decimals(1).range(0.2, 0.6);
+    let one_decimal_num = one_decimal_range.random();
+    let sum = one_decimal_num + two_decimal_num;
+    let question = format!("${one_decimal_num} + {two_decimal_num}$");
+    let answer = format!("${sum}$");
+    let solution = format!(
+        "${one_decimal_num} + {two_decimal_num} = {one_decimal_num:.2} + {two_decimal_num} = {sum}$"
+    );
+    Ok(Problem {
+        id,
+        question,
+        answer,
+        solution,
+        identifiers: vec![one_decimal_num, two_decimal_num],
+        combinations: two_decimal_range.len() * one_decimal_range.len(),
+    })
+}
+
 /// 0,1 - 0,04
 /// Difficulty: 0
 #[problem]
-fn subtract_different_decimals(id: i32, _lang: Language) -> Result<Problem> {
+fn subtract_decimals_one_decimal_first(id: i32, _lang: Language) -> Result<Problem> {
     let larger_num_range = num_gen::decimal().with_decimals(1).range(0.1, 0.9);
     let larger_num = larger_num_range.random();
     let smaller_num_range = num_gen::decimal().with_decimals(2).range(0.01, 0.09);
@@ -40,6 +64,29 @@ fn subtract_different_decimals(id: i32, _lang: Language) -> Result<Problem> {
     let answer = format!("${difference}$");
     let solution =
         format!("${larger_num} - {smaller_num} = {larger_num:.2} - {smaller_num} = {difference}$");
+    Ok(Problem {
+        id,
+        question,
+        answer,
+        solution,
+        identifiers: vec![larger_num, smaller_num],
+        combinations: larger_num_range.len() * smaller_num_range.len(),
+    })
+}
+
+/// 0,75 - 0,2
+/// Difficulty: 0
+#[problem]
+fn subtract_decimals_two_decimals_first(id: i32, _lang: Language) -> Result<Problem> {
+    let larger_num_range = num_gen::decimal().with_decimals(2).range(0.3, 0.9);
+    let larger_num = larger_num_range.random();
+    let smaller_num_range = num_gen::decimal().with_decimals(1).range(0.1, larger_num);
+    let smaller_num = smaller_num_range.random();
+    let difference = larger_num - smaller_num;
+    let question = format!("${larger_num} - {smaller_num}$");
+    let answer = format!("${difference}$");
+    let solution =
+        format!("${larger_num} - {smaller_num} = {larger_num} - {smaller_num:.2} = {difference}$");
     Ok(Problem {
         id,
         question,
